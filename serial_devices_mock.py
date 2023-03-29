9df28f61ecec5571874d187874d1cc1b0747ed4d
Vitor Almeida <vitorcarvalhoamd@gmail.com>
2023-03-28 22:07:08 -0300

    Serial mock devices responding to any messages starting with 'G' or 'M'

--- serial_devices_mock.py
+++ serial_devices_mock.py
@@ -4,12 +4,19 @@ from serial import Serial
 
 def gcode_positioner():
     device_mock = MockSerial()
-    stub = device_mock.stub(
-        receive_bytes=b'G28\n',
+    device_mock.stub(
+        receive_bytes=b'G',
         send_bytes=b'ok\n'
     )
+
+    device_mock.stub(
+        receive_bytes=b'M',
+        send_bytes=b'ok\n'
+    )
+
 
     device_mock.open()
     ser = Serial(device_mock.port)
 
     return ser, device_mock
+
